f07bfd91adb919a46bd386a6a446dad9d8b1e69f
shorzy <shorzy90@gmail.com>
2017-02-28 21:41:13 +0100

    Add test for saving bear to store

---
+++ server/test/store.spec.js
@@ -0,0 +1,23 @@
+import { expect } from 'chai';
+import sinon from 'sinon';
+
+import { createStore } from '../src/store';
+
+describe('the store', () => {
+  let store;
+  let insertSpy;
+
+  before(() => {
+    insertSpy = sinon.spy();
+    const db = {
+      insert: insertSpy
+    }
+    store = createStore(db);
+  });
+
+  it('should save the bear', () => {
+    const document = { name: 'Bamse' };
+    store.saveTeamConfig(document);
+    expect(insertSpy.firstCall.args[0].name).to.equal(document.name);
+  });
+});
