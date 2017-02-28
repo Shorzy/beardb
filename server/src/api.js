256d5d5873acc63a9fa035a58128213a3c71f946
shorzy <shorzy90@gmail.com>
2017-02-28 21:33:10 +0100

    Add api

---
+++ server/src/api.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const { createBearHandler } = require('./handlers/bear-handler');
+
+function createApi(store) {
+  const app = express();
+
+  app.use(bodyParser.json());
+  app.use(express.static(path.join(__dirname, '..', '..', 'public')));
+
+  const bearHandler = createBearHandler(store);
+  app.post('/bears', bearHandler.save);
+  app.get('/bears', bearHandler.get);
+  return app;
+}
+
+module.exports = {createApi };
