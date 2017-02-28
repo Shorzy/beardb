bc218e79aff3d2961b307c82126f0de3a250419e
shorzy <shorzy90@gmail.com>
2017-02-28 22:20:44 +0100

    correct indentation

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,17 @@
 module.exports = {
-    entry: './client/src/index.js',
-    output: {
-        path: 'public',
-        filename: 'bundle.js'
-    },
-    devtool: 'source-map',
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader'
-            }
-        ]
-    }
+  entry: './client/src/index.js',
+  output: {
+    path: 'public',
+    filename: 'bundle.js'
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
 };
