dd0cb8d0496eac5ca875af6a61a1e8283b413500
shorzy <shorzy90@gmail.com>
2017-02-28 20:43:42 +0100

    Add db connection

---
+++ server/src/db-util.js
@@ -0,0 +1,23 @@
+function createIfNotExisting(dbConn, dbName) {
+  return dbConn.db.get(dbName)
+  .then(() => dbConn.use(dbName))
+  .catch(() => dbConn.dbCreate(dbName))
+  .then(() => dbConn.use(dbName));
+}
+
+function getDatabaseViews() {
+  return {
+    _id: '_design/views',
+    views: {
+      bears: {
+        map: function(doc) {
+          if (doc.type === 'bear') {
+            emit(null, doc);
+          }
+        }
+      }
+    }
+  };
+}
+
+module.exports = { createIfNotExisting, getDatabaseViews };
