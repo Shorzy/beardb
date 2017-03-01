22837d1861690fbc34e34403a278dd7c50251534
shorzy <shorzy90@gmail.com>
2017-03-01 20:40:46 +0100

    can now parse secrets file

--- server/src/index.js
+++ server/src/index.js
@@ -2,14 +2,15 @@ const { createApi } = require('./api');
 const { createStore } = require('./store');
 const nano = require('nano-blue');
 const dbUtil = require('./db-util');
+const fs = require('fs');
 
 const VCAP_SERVICES_JSON = process.env.VCAP_SERVICES;
-if (!VCAP_SERVICES_JSON) {
-  throw new Error('Failed to read VCAP_SERVICES. Have you sources the secrets?');
+let vcapServices;
+if (VCAP_SERVICES_JSON) {
+  vcapServices = JSON.parse(VCAP_SERVICES_JSON);
+} else {
+  vcapServices = JSON.parse(fs.readFileSync('./.secrets', 'utf-8'));
 }
-
-const vcapServices = JSON.parse(VCAP_SERVICES_JSON);
-
 const cloudantUrl = vcapServices.cloudantNoSQLDB[0].credentials.url;
 const dbConn = nano(cloudantUrl);
 const dbName = process.env.BEARDB_DB_NAME || 'beardb';
